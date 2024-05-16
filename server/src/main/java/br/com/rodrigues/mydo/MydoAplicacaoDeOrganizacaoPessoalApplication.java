0917a9a35b6205d12e63f2f262844c4785de1516
Paulo Henrique <paulo001.life@gmail.com>
2024-05-15 22:30:23 -0300

    Feat[Geral] - Projetos Iniciais

---
+++ server/src/main/java/br/com/rodrigues/mydo/MydoAplicacaoDeOrganizacaoPessoalApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodrigues.mydo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MydoAplicacaoDeOrganizacaoPessoalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MydoAplicacaoDeOrganizacaoPessoalApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/br/com/rodrigues/mydo/MydoAplicacaoDeOrganizacaoPessoalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigues.mydo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MydoAplicacaoDeOrganizacaoPessoalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
